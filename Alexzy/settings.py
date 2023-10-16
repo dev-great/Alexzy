99e4e6dcd8a74139cff426685601f3a714c02b48
Marshal Greatness <31458994+dev-great@users.noreply.github.com>
2023-10-16 22:52:49 +0100

    Update settings.py

--- Alexzy/settings.py
+++ Alexzy/settings.py
@@ -201,7 +201,11 @@ CLOUDINARY_STORAGE = {
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+
+]
 
 
 # Media files
