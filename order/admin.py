03e36389620e9cf9cb59ddcfadb32a181512e5f7
Marshal Greatness <31458994+dev-great@users.noreply.github.com>
2023-11-08 13:58:50 +0100

    Update admin.py

--- order/admin.py
+++ order/admin.py
@@ -1,6 +1,28 @@
 from .models import *
 from django.contrib import admin
 
-# Register your models here.
-admin.site.register(Cart)
-admin.site.register(CartItem)
+
+class ShippingAddressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'city', 'state')
+    search_fields = ('user__email', 'first_name',
+                     'last_name', 'city', 'state')
+
+
+admin.site.register(ShippingAddress, ShippingAddressAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'total_price', 'order_date')
+    search_fields = ('user__username',)
+
+
+admin.site.register(Order, OrderAdmin)
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'product', 'quantity', 'price')
+    search_fields = ('order__user__username', 'product__product_name')
+
+
+admin.site.register(OrderItem, OrderItemAdmin)
+admin.site.register(OrderStatus)
