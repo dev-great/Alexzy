a4c75dd4b4cd215ac59884f854475f498cf76057
Marshal Greatness <31458994+dev-great@users.noreply.github.com>
2023-12-27 14:53:04 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -58,4 +58,4 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.author.user.email
+        return self.author.email
